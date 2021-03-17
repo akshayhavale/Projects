6cb90a670372588225d62d205d5a9dff128938d9
aksha <aksha@DESKTOP-OBKKHTH>
2021-03-17 21:03:21 +0530

    new project

---
+++ spring-boot-imageUrl-to-image/src/main/java/com/application/SpringBootImageUrlToImageApplication.java
@@ -0,0 +1,13 @@
+package com.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootImageUrlToImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootImageUrlToImageApplication.class, args);
+	}
+
+}
